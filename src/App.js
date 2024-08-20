bc46b6dcba2e7be15c82b3c17304ed984395f877
EngsBreitwisser <engs-brunobreitwisser@camproeal.edu.br>
2024-08-19 21:16:41 -0300

    masterchef código

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import logo from './images/masterchef.jpg';
 
 function App() {
+  const [receitas, setReceitas] = useState([
+    { id: 1, nome: 'Bolo de Chocolate', ingredientes: ['Farinha', 'Açúcar', 'Ovos', 'Chocolate'] },
+    { id: 2, nome: 'Lasanha', ingredientes: ['Massa', 'Molho de Tomate', 'Queijo', 'Carne Moída'] }
+  ]);
+  const [novaReceita, setNovaReceita] = useState({ nome: '', ingredientes: '' });
+
+  const addReceita = () => {
+    const listaDeIngredientes = novaReceita.ingredientes.split(',').map(ingredient => ingredient.trim());
+    const receitaCriada = { id: receitas.length + 1, nome: novaReceita.nome, ingredientes: listaDeIngredientes };
+    setReceitas([...receitas, receitaCriada]);
+    setNovaReceita({ nome: '', ingredientes: '' });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <img src={logo} alt="Logo do masterchef Brunão" width={1560} height={200} />
       </header>
+      <div>
+        {receitas.map((receita) => (
+          <article key={receita.id}>
+            <strong>{receita.nome}</strong>
+            <ul>
+              {receita.ingredientes.map((ingrediente, index) => (
+                <li key={index}>{ingrediente}</li>
+              ))}
+            </ul>
+          </article>
+        ))}
+      </div>
+      <div>
+        <input
+          type="text"
+          placeholder="Nome da Receita"
+          value={novaReceita.nome}
+          onChange={(e) => setNovaReceita({ ...novaReceita, nome: e.target.value })}
+        />
+        <input
+          type="text"
+          placeholder="Ingredientes separados por vírgula. EX: batata, açúcar, pão"
+          value={novaReceita.ingredientes}
+          onChange={(e) => setNovaReceita({ ...novaReceita, ingredientes: e.target.value })}
+        />
+        <button onClick={addReceita}>Adicionar Receita</button>
+      </div>
     </div>
   );
 }
